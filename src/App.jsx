dca9cef10d43d32fa862e580b6eb3c8a99cd8aca
KAMBAYASHI Yuya <kambayashi73@gmail.com>
2021-08-04 12:00:07 +0900

    cssを実装します

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,29 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>aaa</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>bbb</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>ccc</li>
             <button>戻す</button>
           </div>
