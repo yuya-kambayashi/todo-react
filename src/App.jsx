51692581b71ed05bf595e58ccf58987a1269e44f
KAMBAYASHI Yuya <kambayashi73@gmail.com>
2021-08-04 11:56:30 +0900

    htmlのマークアップ

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>aaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>bbb</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>ccc</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
