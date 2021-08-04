f17409ee3e91bb32d86c5c3da14245af51bdadd5
KAMBAYASHI Yuya <kambayashi73@gmail.com>
2021-08-04 13:05:26 +0900

    InputTextをコンポーネント化します

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css";
+import { InputTodo } from "./components/InputTodo";
 
 export const App = () => {
   const [incompleteTodos, setIncompleteTodos] = useState(["aaas", "bbb"]);
@@ -49,14 +50,11 @@ export const App = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeTodoText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
